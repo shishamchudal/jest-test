6f64c991ec7274b4591350dc7ed7e0f1033d93c3
Shisham Chudal <shishamchudal61@gmail.com>
2025-04-20 17:15:32 -0700

    feat: replace HelloWorld component with MessageComponent in App.vue

--- vueapp-test/my-app/src/App.vue
+++ vueapp-test/my-app/src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <MessageComponent msg="This is my first VUE app!" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MessageComponent from "./components/MessageComponent.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    MessageComponent,
+  },
+};
 </script>
 
 <style>
